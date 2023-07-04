76355e0a0ec02a2c93ddfd0c06e3137e754db786
xiexixie <xiejingnan937@qq.com>
2023-07-04 12:43:54 +0800

    paeser design

--- xie/parser.cpp
+++ xie/parser.cpp
@@ -14,7 +14,9 @@ void Parser::load(const std::string &str)
 void Parser::skip_blank()
 {
   while (m_str[m_index] == ' ' || m_str[m_index] == '\n' || m_str[m_index] == '\r' || m_str[m_index] == '\t')
+  {
     m_index++;
+  }
 }
 char Parser::get_next_token()
 {
@@ -50,8 +52,10 @@ Json Parser::parse()
   case '"':
     return Json(parse_string());
   case '[':
+    m_index--;
     return parse_array();
   case '{':
+    // m_index--;
     return parse_obj();
 
   default:
@@ -62,15 +66,24 @@ Json Parser::parse()
 Json Parser::parse_null()
 {
   if (m_str.compare(m_index, 4, "null") == 0)
+  {
+    m_index += 4;
     return Json();
+  }
   throw new std::logic_error("parse null error");
 }
 Json Parser::parse_bool()
 {
   if (m_str.compare(m_index, 4, "true") == 0)
+  {
+    m_index += 4;
     return Json(true);
+  }
   else if (m_str.compare(m_index, 5, "false") == 0)
+  {
+    m_index += 5;
     return Json(false);
+  }
   throw new std::logic_error("parse bool error");
 }
 Json Parser::parse_number()
@@ -97,13 +110,95 @@ Json Parser::parse_number()
 }
 Json Parser::parse_array()
 {
-  return Json();
+  Json arr = Json(Json::json_array);
+  char ch = get_next_token();
+  // m_index--;
+  if (ch == ']')
+    return arr;
+  // m_index--;
+  while (true)
+  {
+    arr.append(parse());
+    ch = get_next_token();
+    if (ch == ']')
+      break;
+    if (ch != ',')
+      throw new std::logic_error("parse array error");
+    skip_blank();
+  }
+  return arr;
 }
 Json Parser::parse_obj()
 {
-  return Json();
+  Json obj(Json::json_object);
+  char ch = get_next_token();
+  if (ch == '}')
+    return obj;
+  m_index--;
+  while (true)
+  {
+    ch = get_next_token();
+    if (ch != '"')
+      throw new std::logic_error("parse obj error");
+    std::string key = parse_string();
+    ch = get_next_token();
+    if (ch != ':')
+      throw new std::logic_error("parse obj error");
+    obj[key] = parse();
+    ch = get_next_token();
+    if (ch == '}')
+      break;
+    if (ch != ',')
+      throw new std::logic_error("parse obj error");
+    skip_blank();
+  }
+  return obj;
 }
 std::string Parser::parse_string()
 {
-  return std::string("");
+  std::string out;
+  while (true)
+  {
+    char ch = m_str[m_index++];
+    if (ch == '"')
+      break;
+    if (ch == '\\')
+    {
+      ch = m_str[m_index++];
+      switch (ch)
+      {
+      case '\n':
+        out += '\n';
+        break;
+      case '\t':
+        out += '\t';
+        break;
+      case '\r':
+        out += '\r';
+        break;
+      case '\b':
+        out += '\b';
+        break;
+      case '\f':
+        out += '\f';
+        break;
+      case '"':
+        out += "\\\"";
+        break;
+      case '\\':
+        out += "\\\\";
+        break;
+      case 'u':
+        out += "\\u";
+        for (int i = 0; i < 4; i++)
+          out += m_str[m_index++];
+        break;
+      default:
+        break;
+      }
+    }
+    else
+      out += ch;
+  }
+  return out;
 }
